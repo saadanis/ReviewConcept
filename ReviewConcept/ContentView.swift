aa7e5c6a18a2d18b4c566403416029ff9ec9dc89
Saad Anis <saadmohammedanis@gmail.com>
2020-08-06 00:22:07 +0300

    Initial Commit.

---
+++ ReviewConcept/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReviewConcept
+//
+//  Created by Saad Anis on 6/8/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReviewConcept/ReviewConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewConceptApp.swift
+//  ReviewConcept
+//
+//  Created by Saad Anis on 6/8/2020.
+//
+
+import SwiftUI
+
+@main
+struct ReviewConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
